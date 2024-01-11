1420f7b661364e751fcb1590a3e5972d5dd17c53
Mathis Albertzard <malbertzard@gmail.com>
2024-01-11 23:17:53 +0100

    feat: Added implementation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TmuxPeasant
- A tool to work with git and tmux repos for the peasant that doesnt have fzf
+ A tool to work with git and tmux repos for the peasant that doesn't have fzf
+
+Inspired by [ThePrimagen](https://github.com/ThePrimeagen/.dotfiles/blob/master/bin/.local/scripts/tmux-sessionizer) and his tool sessionizer
+
+This does not use fzf. I handle all inputs with linux core utils. So it should run pretty much anywhere as long as tmux is installed.
---
+++ tmux-peasant.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+show_help() {
+    echo "Usage: $(basename "$0") [--help] [search_pattern]"
+    echo
+    echo "Options:"
+    echo "  --help         Show this help message and exit"
+    echo "  search_pattern Fuzzy search pattern to filter results"
+    echo
+    echo "Tipp:"
+    echo "  ending the search_pattern with \$ signals grep that the pattern should end here"
+}
+
+while getopts ":h-:" opt; do
+    case $opt in
+        -)
+            case "${OPTARG}" in
+                help)
+                    show_help
+                    exit 0
+                    ;;
+                *)
+                    show_help >&2
+                    exit 1
+                    ;;
+            esac
+            ;;
+        h)
+            show_help
+            exit 0
+            ;;
+        *)
+            show_help >&2
+            exit 1
+            ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    complete_list=$(find ~/GitHub/ ~/personal/ -type d -name .git -exec dirname {} \; 2>/dev/null | grep -F -v -e "(^./vendor/|^./var/|^./node_module/)")
+    last_input=""
+
+    echo "$complete_list"
+
+    while true; do
+        echo
+        echo -n "Enter a fuzzy search pattern: "
+        read -e -r -i "$last_input" fuzzy_pattern
+
+        filtered_list=$(echo "$complete_list" | grep --line-buffered -i "$fuzzy_pattern")
+
+        if [ -z "$filtered_list" ]; then
+            echo
+            echo "No matching results found."
+            continue
+        fi
+
+        if [ $(echo "$filtered_list" | wc -l) -eq 1 ]; then
+            echo
+            echo "Selected result: $filtered_list"
+            selected=$filtered_list
+            break
+        fi
+
+        echo
+        echo "Matching results:"
+        echo "$filtered_list"
+
+        # Save the current input as the last input for the next iteration
+        last_input="$fuzzy_pattern"
+    done
+fi
+
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+selected_name=$(basename "$selected" | tr . _)
+tmux_running=$(pgrep tmux)
+
+if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+    tmux new-session -s $selected_name -c $selected "~/nvim.appimage ."
+    exit 0
+fi
+
+if ! tmux has-session -t=$selected_name 2> /dev/null; then
+    tmux new-session -ds $selected_name -c $selected "~/nvim.appimage ."
+fi
+
+tmux switch-client -t $selected_name
